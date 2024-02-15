8169918cae0c374d93d49ce2b0071bd2d41468f5
GitHub Action <action@github.com>
2024-02-15 10:44:40 +0000

    Added generated code for LockDealNFT

---
+++ LockDealNFT/ContractDefinition/BasePoolInfo.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Numerics;
+using Nethereum.Hex.HexTypes;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+
+namespace poolz.finance.csharp.LockDealNFT.ContractDefinition
+{
+    public partial class BasePoolInfo : BasePoolInfoBase { }
+
+    public class BasePoolInfoBase 
+    {
+        [Parameter("address", "provider", 1)]
+        public virtual string Provider { get; set; }
+        [Parameter("string", "name", 2)]
+        public virtual string Name { get; set; }
+        [Parameter("uint256", "poolId", 3)]
+        public virtual BigInteger PoolId { get; set; }
+        [Parameter("uint256", "vaultId", 4)]
+        public virtual BigInteger VaultId { get; set; }
+        [Parameter("address", "owner", 5)]
+        public virtual string Owner { get; set; }
+        [Parameter("address", "token", 6)]
+        public virtual string Token { get; set; }
+        [Parameter("uint256[]", "params", 7)]
+        public virtual List<BigInteger> Params { get; set; }
+    }
+}
