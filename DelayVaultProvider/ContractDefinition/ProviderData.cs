b4f60105ef3163e72bf7c368257a413d0b207359
GitHub Action <action@github.com>
2024-02-15 12:35:10 +0000

    Added generated code for DelayVaultProvider

---
+++ DelayVaultProvider/ContractDefinition/ProviderData.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Numerics;
+using Nethereum.Hex.HexTypes;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+
+namespace poolz.finance.csharp.DelayVaultProvider.ContractDefinition
+{
+    public partial class ProviderData : ProviderDataBase { }
+
+    public class ProviderDataBase 
+    {
+        [Parameter("address", "provider", 1)]
+        public virtual string Provider { get; set; }
+        [Parameter("uint256[]", "params", 2)]
+        public virtual List<BigInteger> Params { get; set; }
+        [Parameter("uint256", "limit", 3)]
+        public virtual BigInteger Limit { get; set; }
+    }
+}
